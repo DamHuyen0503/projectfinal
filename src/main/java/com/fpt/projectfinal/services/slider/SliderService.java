e78ec5d3a82619334282296f862affdf3997e7d4
VuThiThuyB <thuyvtse05107@fpt.edu.vn>
2019-07-25 00:06:46 +0700

    CRUD Slider

---
+++ src/main/java/com/fpt/projectfinal/services/slider/SliderService.java
@@ -0,0 +1,15 @@
+package com.fpt.projectfinal.services.slider;
+
+import java.util.List;
+
+import com.fpt.projectfinal.models.Slider;
+
+public interface SliderService {
+	public void addSlider(Slider slider) throws Exception;
+
+	public List<Slider> getAllSlider();
+	
+	public void updateSlider(Slider slider);
+	
+	public void deleteSlider(Integer sliderID);
+}
